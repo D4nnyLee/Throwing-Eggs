62ed2a6a68cd9d2b34e361e0f44bc149fb1a6c9a
D4nnyLee <zxcvbnm04987@gmail.com>
2020-12-27 20:58:02 +0800

    Disable debug mode and change `ALLOWED_HOST` to heroku's URL

--- ThrowingEggs/settings.py
+++ ThrowingEggs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cma22lfdw)ru1k=5=dfrgo%1m1x2zasor#u&_m@$nvcv)=86mj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://throwingeggs.herokuapp.com']
 
 
 # Application definition
