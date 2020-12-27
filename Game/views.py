18f36f76d52040e31836b688e1307ec330744e65
D4nnyLee <zxcvbnm04987@gmail.com>
2020-12-27 18:30:34 +0800

    Fix deploy problem about static file

--- Game/views.py
+++ Game/views.py
@@ -129,6 +129,6 @@ def get_fsm(request):
         show_conditions = show_conditions,
     )
 
-    machine.get_graph().draw('static/Game/fsm.png', prog = 'dot', format = 'png')
+    machine.get_graph().draw('Game/static/fsm.png', prog = 'dot', format = 'png')
 
     return render(request, 'show_fsm.html')
